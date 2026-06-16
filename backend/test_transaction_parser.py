413884beac18cdb573c954d180027aec720c8301
Thanu Venu <thanu.venu28@gmail.com>
2026-06-16 15:41:33 +0530

    Support basic NDB parsing

--- backend/test_transaction_parser.py
+++ backend/test_transaction_parser.py
@@ -1,7 +1,7 @@
 from app.services.pdf_parser import extract_text
 from app.services.transaction_parser import parse_transactions
 
-text = extract_text("test_data/sample.pdf")
+text = extract_text("test_data/multi-line.pdf")
 
 transactions=parse_transactions(text)
 
