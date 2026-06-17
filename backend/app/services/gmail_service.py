b82378b2d7569ab838789250e71c86963310cad8
Thanu Venu <thanu.venu28@gmail.com>
2026-06-17 11:38:56 +0530

    Implement gmail statement download and processing

--- backend/app/services/gmail_service.py
+++ backend/app/services/gmail_service.py
@@ -1,5 +1,6 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+import base64
 
 def get_gmail_service(token):
 
@@ -25,3 +26,35 @@ def get_message_details(service,message_id):
         .execute()
     )
     return message
+
+def get_message(service,message_id):
+    return(
+        service.users()
+        .messages()
+        .get(
+            userId="me",
+            id=message_id
+        )
+        .execute()
+    )
+
+def download_attachment(
+    service,
+    message_id,
+    attachment_id
+):
+    attachment=(
+        service.users()
+        .messages()
+        .attachments()
+        .get(
+            userId="me",
+            messageId=message_id,
+            id=attachment_id
+        )
+        .execute()
+    )
+
+    data=attachment["data"]
+    file_data=base64.urlsafe_b64decode(data)
+    return file_data
