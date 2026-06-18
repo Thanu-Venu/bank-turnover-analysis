d7c43426834420b74e2398783519532454e522d6
Thanu Venu <thanu.venu28@gmail.com>
2026-06-18 08:43:31 +0530

    Detecting different type of account and only selecting current account for turnover report calculation of monthly and yearly

--- backend/app/api/reports.py
+++ backend/app/api/reports.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
-from app.services.report_service import yearly_turnover
+from app.services.report_service import yearly_turnover,get_monthly_report
 
 router=APIRouter()
 
 @router.get("/reports/yearly/{year}")
 def get_yearly_report(year:int):
     return yearly_turnover(year)
+
+@router.get("/reports/monthly/{year}/{month}")
+def monthly_report(year:int,month:int):
+    return get_monthly_report(year,month)
